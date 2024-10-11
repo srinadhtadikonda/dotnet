e7a2125cc004b9109349b0abac9998778c43640c
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-10-11 09:57:41 +0530

    Update interface.cs

--- C#/interface.cs
+++ C#/interface.cs
@@ -1,1 +1,28 @@
-
+interface A 
+{
+void greetone();   
+}
+interface B 
+{
+    void greettwo();
+}
+class C:A,B 
+{
+    public void greetone()
+    {
+        System.Console.WriteLine("Hai");
+    }
+    public void greettwo()
+    {
+        System.Console.WriteLine("How are you");
+    }
+}
+class Test 
+{
+    static void Main()
+    {
+        C objC=new C();
+        objC.greetone();
+        objC.greettwo();
+    }
+}
