6a0285189036b9314872adf4a10cf963e950489b
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-12-02 17:32:20 +0530

    Update jaggedarray.cs

--- C#/jaggedarray.cs
+++ C#/jaggedarray.cs
@@ -1,34 +1,26 @@
-
-// C# program to illustrate the declaration
-// and Initialization of Jagged Arrays
 using System;
-
-class GFG {
-
-    // Main Method
+class Test
+{
     public static void Main()
     {
-
-        // Declare the Jagged Array of four elements:
-        int[][] jagged_arr = new int[4][];
+        int[][]a = new int[4][];
 
         // Initialize the elements
-        jagged_arr[0] = new int[] { 1, 2, 3, 4 };
-        jagged_arr[1] = new int[] { 11, 34, 67 };
-        jagged_arr[2] = new int[] { 89, 23 };
-        jagged_arr[3] = new int[] { 0, 45, 78, 53, 99 };
+       a[0] = new int[] { 1, 2, 3, 4 };
+       a[1] = new int[] { 11, 34, 67 };
+       a[2] = new int[] { 89, 23 };
+       a[3] = new int[] { 0, 45, 78, 53, 99 };
 
         // Display the array elements:
-        for (int n = 0; n < jagged_arr.Length; n++) {
+        for (int n = 0; n <a.Length; n++) {
 
             // Print the row number
             System.Console.Write("Row({0}): ", n);
 
-            for (int k = 0; k < jagged_arr[n].Length; k++) {
+            for (int k = 0; k <a[n].Length; k++) {
 
                 // Print the elements in the row
-                System.Console.Write("{0} ",
-                                     jagged_arr[n][k]);
+                System.Console.Write("{0} ",a[n][k]);
             }
             System.Console.WriteLine();
         }
