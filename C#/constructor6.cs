4928e0669bcce15d8c5085fd74973adf2e59e903
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-04-04 09:38:44 +0530

    Update constructor6.cs

--- C#/constructor6.cs
+++ C#/constructor6.cs
@@ -1,39 +1,20 @@
+//private constructor
 using System;
 
-class Rect
+class MyMath
 {
-    public int l;
-    public int b;
-    
-    private Rect()  // private constructor
-    {
-        l = 20;
-        b = 10;
-    }
+    private MyMath() {} // prevent object creation
 
-    public static Rect CreateInstance() // static method to create instance
+    public static int Add(int a, int b)
     {
-        return new Rect();
-    }
-
-    public void putVals()
-    {
-        System.Console.WriteLine("Length=" + l);
-        System.Console.WriteLine("Breadth=" + b);
-    }
-
-    public int calArea()
-    {
-        return l * b;
+        return a + b;
     }
 }
 
-class MyRect
+class Test
 {
     static void Main()
     {
-        Rect R = Rect.CreateInstance(); // use static method to create instance
-        R.putVals();
-        System.Console.WriteLine("Area of Rectangle=" + R.calArea());
+        Console.WriteLine(MyMath.Add(5, 3)); // direct use
     }
 }
