e0396e8505d950c9ce98fb2f8577e22a6c642067
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-04-11 10:50:50 +0530

    Implement method overriding for Animal and Dog classes

--- C#/overriding1.cs
+++ C#/overriding1.cs
@@ -1,3 +1,5 @@
+using System;
+
 class Animal
 {
     public virtual void Sound()
@@ -5,10 +7,20 @@ class Animal
         Console.WriteLine("Animal makes sound");
     }
 }
+
 class Dog : Animal
 {
     public override void Sound()
     {
         Console.WriteLine("Dog barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal a = new Dog();
+        a.Sound();   // Output: Dog barks
     }
 }
